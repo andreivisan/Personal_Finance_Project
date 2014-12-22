3b59a83b9b32f69ea4b634e6d936a5ea31d03a35
Andrei Visan <andreivisan@Andreis-MBP.home>
2014-12-22 23:13:22 +0100

    Started working at dashboard

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', routes);
+app.use('/dashboard', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Sign In' });
 });
 
+/* GET dashboard. */
+router.get('/dashboard', function(req, res) {
+  res.render('dashboard', { title: 'Dashboard' });
+});
+
 module.exports = router;
