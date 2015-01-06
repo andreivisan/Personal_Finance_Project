b71a7823d8df7789c9d8ee148de8f20b04382b0f
Andrei Visan <andrei.m.visan@gmail.com>
2015-01-06 11:45:32 +0100

    Small Refactoring

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'url' : 'mongodb://andreivisan:Cybermaster2000@ds051640.mongolab.com:51640/personalbudget'
+  'url' : 'mongodb://<dbUser>:<dbPasword>@ds051640.mongolab.com:51640/personalbudget'
 };
